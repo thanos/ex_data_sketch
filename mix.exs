95ede753d210caec5ea27e66f036cd721005c31b
thanos <thanosv@gmail.com>
2026-03-03 12:38:14 -0500

    bumped version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ExDataSketch.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.0-alpha.12"
   @source_url "https://github.com/thanos/ex_data_sketch"
 
   def project do
